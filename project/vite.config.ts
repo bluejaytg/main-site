8f7ab5c0e99f11f266f706911218d1186dbab4ca
bluejaytg <jared@bluejaytg.com>
2025-03-28 18:23:43 -0400

    main site update

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir: "build", // Change output folder from "dist" to "build"
+    outDir: "dist", // Change output folder from "dist" to "build"
   },
 });
